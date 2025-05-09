b3539fde871cf8331202f78be38f0cbb8e06e1f3
sophia-bergamo <sophiabergamo7@gmail.com>
2025-05-09 01:03:33 -0300

    atomic password input

--- src/atomic/mol.input-fields/input-field.component.tsx
+++ src/atomic/mol.input-fields/input-field.component.tsx
@@ -2,7 +2,7 @@ import { useFormContext } from "react-hook-form";
 import { Text } from "../atm-typography/typography.component";
 import { Input } from "../atm-input/input.component";
 
-interface InputFieldProps {
+export interface InputFieldProps {
   name: string;
   label: string;
   type: "text" | "email" | "password";
@@ -25,12 +25,7 @@ export function InputField({
   return (
     <div className={className}>
       <Text variant="inputLabel">{label}</Text>
-      <Input
-        variant="primary"
-        type={type}
-        {...register(name)}
-        placeholder={placeholder}
-      />
+      <Input type={type} {...register(name)} placeholder={placeholder} />
       {errors[name]?.message && (
         <Text variant="inputCaptionError">{String(errors[name]?.message)}</Text>
       )}
